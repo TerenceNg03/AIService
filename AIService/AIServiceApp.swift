5a43a64c9f87b635a221e82255cdeb4457ddad1c
TerenceNg03 <terenceng03@outlook.com>
2024-10-04 12:44:24 +0200

    Initial Commit

---
+++ AIService/AIServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIServiceApp.swift
+//  AIService
+//
+//  Created by Terence Ng on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct AIServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
